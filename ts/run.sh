26ff7c391f3e2081cab96c965bfcf4982eb21566
Emily Shi <code@emi.sh>
2019-02-21 22:45:59 -0500

    working again oops

--- ts/run.sh
+++ ts/run.sh
@@ -1,3 +1,4 @@
 export CUR=$(pwd)
+export TEST_FILE="$(cd ..; pwd)/$1"
 cd $JALANGI_PATH
-mx jalangi --analysis "$CUR/dist/index.js" "$CUR/$2"
+mx jalangi --analysis "$CUR/dist/index.js" "$TEST_FILE"
