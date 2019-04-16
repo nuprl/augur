ac9f58a77884e70a9f966f2e05b35d6fab3e2237
Emily Shi <code@emi.sh>
2019-04-16 03:36:23 -0400

    loose change

--- ts/run-callbacks.sh
+++ ts/run-callbacks.sh
@@ -2,4 +2,4 @@ export CUR=$(pwd)
 export SOURCES=$2
 export SINKS=$3
 cd $NODEPROF_PATH
-mx jalangi --analysis "$CUR/dist/emptyAnalysis.js" "$CUR/$1"
+mx jalangi --analysis "$CUR/dist/src/emptyAnalysis.js" "$CUR/$1"
