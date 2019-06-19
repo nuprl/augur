b64dfe8ba6853117910d90bdb92b0302efb65c81
Mark Aldrich <mastermark51200@gmail.com>
2019-06-19 14:18:34 -0400

    specifying an output file is officially mandatory

--- ts/docker-run.sh
+++ ts/docker-run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 usage() {
-    echo "Usage: ./docker-run.sh [-h|--help] --inputFile <file to instrument> [--outputFile <path to desired analysis output>] [--sources <comma-separated sources list>] [--sinks <comma-separated sinks list]"
+    echo "Usage: ./docker-run.sh [-h|--help] --inputFile <file to instrument> --outputFile <path to desired analysis output> [--sources <comma-separated sources list>] [--sinks <comma-separated sinks list]"
 }
 
 CUR=""
@@ -56,10 +56,9 @@ export CUR
 export SOURCES
 export SINKS
 
-
 # If the user did not specify the program to instrument, print the usage and
 # terminate.
-if [[ -z "$INPUT_FILE" ]]
+if [[ -z "$INPUT_FILE" ]] || [[ -z "$OUTPUT_FILE" ]]
 then
     usage
     exit 1
@@ -69,41 +68,24 @@ fi
 # INPUT_FILE and OUTPUT_FILE are absolute. We can do this by canonicalizing
 # both paths.
 canonicalize() {
-    readlink -f $@
+    readlink -f "$@"
 }
 INPUT_FILE=`canonicalize "$INPUT_FILE"`
 OUTPUT_FILE=`canonicalize "$OUTPUT_FILE"`
 
-# if [[ ! "$INPUT_FILE" = /* ]]
-# then
-#     INPUT_FILE=`readlink -f $INPUT_FILE` # ${CUR}/${INPUT_FILE}
-# fi
-# if [[ ! "$OUTPUT_FILE" = /* ]] && [[ -z "$OUTPUT_FILE" ]]
-# then
-#     OUTPUT_FILE=${CUR}/${OUTPUT_FILE}
-# fi
-
-# If an output file was specified, we need to pass this along to the `mx`
-# command.
-if [[ -z "$OUTPUT_FILE" ]]
-then
-    MX_OPTIONS=""
-    DOCKER_OPTIONS=""
-else
-    MX_OPTIONS="--initParam outputFile:/root/output.js"
-    touch "$OUTPUT_FILE"
-    DOCKER_OPTIONS="-v $OUTPUT_FILE:/root/output.js"
-fi
+# Ensure the output file exists before we attempt to mount it inside the
+# Docker container.
+touch "$OUTPUT_FILE"
 
 docker run --rm \
        -v $CUR:/root/ts \
        -v $INPUT_FILE:/root/program.js \
-       ${DOCKER_OPTIONS} \
+       -v $OUTPUT_FILE:/root/output.js \
        -e SOURCES="$SINKS" \
        -e SINKS="$SOURCES" \
        jsanalysis:latest \
        bash -c \
        "(cd /root/ts; \
        /root/mx/mx -p /root/nodeprof/ jalangi \
-       $MX_OPTIONS \
+         --initParam outputFile:/root/output.js \
          --analysis /root/ts/dist/src/analysis/index.js /root/program.js)"
