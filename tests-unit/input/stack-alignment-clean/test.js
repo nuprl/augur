18d3ed9f7f4ac929f386909e3589a8e2b53b0b81
Mark Aldrich <mastermark51200@gmail.com>
2019-07-01 11:45:12 -0400

    add explanatory comments to stack alignment test

--- tests-unit/input/stack-alignment-clean/test.js
+++ tests-unit/input/stack-alignment-clean/test.js
@@ -2,8 +2,10 @@
 // will fail. `a` will be left on the stack and be used in place of `1` in the
 // call to `first`.
 
+// The taint source.
 var a = 0;
 
+// A function that assigns a tainted value to a variable.
 function inc(x) {
     x = a;
     return 1;
@@ -13,4 +15,10 @@ function first(g, h) {
     return g;
 }
 
+// If the tainted value of the assignment expression in `inc` is not
+// discarded, it will leak into the stack, and take the place of the
+// untainted value `1`.
+//
+// Then, when it is returned by `first`, `z` will be tainted, which it
+// shouldn't be.
 var z = first(1, inc(1));
