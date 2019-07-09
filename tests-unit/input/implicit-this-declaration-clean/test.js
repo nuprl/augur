3f0915a5f47c14e2c9bf1563640602611532ddd8
Mark Aldrich <mastermark51200@gmail.com>
2019-07-09 14:18:53 -0400

    add explanation for implicit `this` declaration test

--- tests-unit/input/implicit-this-declaration-clean/test.js
+++ tests-unit/input/implicit-this-declaration-clean/test.js
@@ -1,3 +1,8 @@
+// The assignment expression, `z = this`, generates instructions to pop the
+// stack and write this to `z`. Prior to this expression, `height` was pushed
+// onto the stack. If no instruction is generated for `this`, `height` is
+// used instead, resulting in a tainted variable taking the place of `this`.
+
 let a = 2;
 let z = 3;
 
