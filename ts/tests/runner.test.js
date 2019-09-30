2cd329826a97ce2576e6675b5b53cd45db1968e2
Mark Aldrich <mastermark51200@gmail.com>
2019-09-30 13:08:28 -0400

    made promise-then-tainted more precise

--- ts/tests/runner.test.js
+++ ts/tests/runner.test.js
@@ -162,10 +162,10 @@ test('init-destructure-obj-clean', (done) => runTest('init-destructure-obj-clean
 test('promise-await-clean', (done) => runTest('promise-await-clean', done));
 test('promise-await-tainted', (done) => runTest('promise-await-tainted', done));
 test('promise-then-clean', (done) => runTest('promise-then-clean', done));
+test('promise-then-tainted', (done) => runTest('promise-then-tainted', done))
 test('async-await-tainted', (done) => runTest('async-await-tainted', done));
 test('async-then-tainted', (done) => runTest('async-then-tainted', done));
 test('init-destructure-obj-tainted', (done) => runTest('init-destructure-obj-tainted', done));
-test('promise-then-tainted', (done) => runTest('promise-then-tainted', done))
 
 // New tests
 test('computed-property-name-tainted', (done) => runTest('computed-property-name-tainted', done));
