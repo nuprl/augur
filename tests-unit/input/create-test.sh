9b81eae12e4a091564e711d89f25b6fc20c6c9c7
Mark Aldrich <mastermark51200@gmail.com>
2019-10-08 13:06:54 -0400

    reformatted create-test.sh script to work in more shells

--- tests-unit/input/create-test.sh
+++ tests-unit/input/create-test.sh
@@ -1,1 +1,5 @@
-for f in clean tainted; do cp -r "template-$f" "$1-$f"; echo "test('$1-$f', (done) => runTest('$1-$f', done));" >> ../../ts/tests/runner.test.js; done
+#!/bin/sh
+for f in clean tainted; do
+    cp -r "template-$f" "$1-$f";
+    echo "test('$1-$f', (done) => runTest('$1-$f', done));" >> ../../ts/tests/runner.test.js;
+done
