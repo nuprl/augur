e5d5ccfbace0b1a985671dfd2d771726f74e1f29
Mark Aldrich <mastermark51200@gmail.com>
2020-06-12 14:03:37 -0400

    stop trying to convert every object to a string in shadow memory

--- ts/src/analysis/shadow/weakMapShadow.ts
+++ ts/src/analysis/shadow/weakMapShadow.ts
@@ -25,7 +25,9 @@ export default class WeakMapShadow implements ShadowMemory {
         let key = this.map.get(o);
 
         if (!key) {
-            console.error(`tried to get non-existent shadow ID for object ${o.toString()}`);
+            // console.error(`tried to get non-existent shadow ID for object
+            // ${o.toString()}`);
+            console.error(`tried to get non-existent shadow ID for object`);
             console.error(`initializing now...`);
             this.initialize(o);
             key = this.map.get(o);
