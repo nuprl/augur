c2f8b2b2433bfbac12b77b4faf217f4e9198deb2
Mark Aldrich <mastermark51200@gmail.com>
2020-06-12 15:06:57 -0400

    add usage to the test creation script

--- tests-unit/input/create-test.sh
+++ tests-unit/input/create-test.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# USAGE: ./create-test.sh "test name"
+# does the following:
+# 1. creates two tests:
+#     1. "test name"-clean
+#     2. "test name"-tainted
+# 2. adds these tests to the runner script
+
 for f in clean tainted; do
     cp -r "template-$f" "$1-$f";
     echo "test('$1-$f', (done) => runTest('$1-$f', done));" >> ../../ts/tests/runner.test.js;
