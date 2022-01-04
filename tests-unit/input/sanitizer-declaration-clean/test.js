4ad7fdaf4ad40180ab791ba555e7fbfc0a1272a3
Mark Aldrich <mark@mwaldrich.io>
2022-01-04 15:38:46 -0500

    removed sanitizer declaration tests because they make no sense.
    you cant sanitize a declaration. only variable reads/writes

--- tests-unit/input/sanitizer-declaration-clean/test.js
+++
@@ -1,6 +0,0 @@
-const a = 2;
-
-const sanitizedDeclaration = a;
-
-// Untainted, because the value assigned to `z` passes through the sanitized declaration
-const z = sanitizedDeclaration;
--- tests-unit/input/sanitizer-declaration-tainted/test.js
+++
@@ -1,6 +0,0 @@
-const a = 2;
-
-const sanitizedDeclaration = a;
-
-// Tainted, because the value assigned to `z` doesn't pass through the sanitized declaration
-const z = a;
