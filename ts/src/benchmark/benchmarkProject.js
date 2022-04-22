7dc7edf8ca890922505dd4a85bdeb78497583e24
Mark Aldrich <mark@mwaldrich.io>
2022-04-22 14:53:21 -0400

    modified benchmark parameters again...

--- ts/src/benchmark/benchmarkProject.js
+++ ts/src/benchmark/benchmarkProject.js
@@ -23,7 +23,7 @@ const experimentName = `benchmark-${testName}-${experiment? "experiment" : "cont
 // const warmRuns = 1000
 const warmRuns = 0
 // const testRuns = 1000
-const testRuns = 200
+const testRuns = 20
 
 // Step 0: Set up Performance Observer
 const experimentResults = [] // Array<PerformanceEntry>
